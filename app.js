f6183bc714b66ad1db8bcd295a19373e9544f577
Luther <luther0211@hotmail.com>
2019-01-01 19:49:05 -0600

    Data Update: Added 'Delete' method to app controller, to delete incomes/expenses from DATA

--- app.js
+++ app.js
@@ -59,6 +59,20 @@ var budgetController = (function() {
       return newItem
     },
 
+    deleteItem: function (type, id) {
+      var ids, index
+      ids = data.allItems[type].map(function(curr, inx, arr) {
+         return curr.id
+      })
+
+      index = ids.indexOf(id);
+
+      if(index !== -1) {
+        data.allItems[type].splice(index, 1)
+      }
+
+    },
+
     calculateBudget: function() {
 
       // calculate total income & expenses
@@ -263,10 +277,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     if(itemID) {
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]) ;
 
       //1 delete item from data structure
-
+      budgetCtrl.deleteItem(type, ID)
 
       //2 delete item from UI
 
